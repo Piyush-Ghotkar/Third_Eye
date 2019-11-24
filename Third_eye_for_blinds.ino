4888d0c7bfbf1c6b02cac2185d11aae2a120d0bc
Piyush Ghotkar <42354510+Piyush-Ghotkar@users.noreply.github.com>
2019-11-25 00:43:28 +0530

    Add files via upload

---
+++ Third_eye_for_blinds.ino
@@ -0,0 +1,42 @@
+#include <Ultrasonic.h>
+
+#include <NewPing.h>
+
+
+
+  const int   pingTrigPin = 12; //Trigger connected to PIN 12   
+  const int pingEchoPin = 10; //Echo connected yo PIN 10  
+  int buz=5; //Buzzer to PIN 5  
+  void setup() {   
+  Serial.begin(9600);   
+  pinMode(buz, OUTPUT);   
+  }   
+  void loop()   
+  {   
+  long duration, cm;   
+  pinMode(pingTrigPin, OUTPUT);   
+  digitalWrite(pingTrigPin, LOW);   
+  delayMicroseconds(2);   
+  digitalWrite(pingTrigPin, HIGH);   
+  delayMicroseconds(5);   
+  digitalWrite(pingTrigPin, LOW);   
+  pinMode(pingEchoPin, INPUT);   
+  duration = pulseIn(pingEchoPin, HIGH);   
+  cm = microsecondsToCentimeters(duration);   
+  if(cm<=50 && cm>0)   
+  {   
+  int d= map(cm, 1, 100, 20, 2000);   
+  digitalWrite(buz, HIGH);   
+  delay(100);   
+  digitalWrite(buz, LOW);   
+  delay(d);  
+  }   
+  Serial.print(cm);    
+  Serial.print("cm");   
+  Serial.println();   
+  delay(100);   
+  }   
+  long microsecondsToCentimeters(long microseconds)   
+  {   
+  return microseconds / 29 / 2;   
+  }   
